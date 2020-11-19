791fa30028ebdcfc00d2a3726b8df4a328184ab5
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-19 15:26:24 +0200

    added solved task-04

--- task-04.js
+++ task-04.js
@@ -10,19 +10,18 @@ class StringBuilder {
   append(str) {
     this._value += str;
   }
+
   prepend(str) {
     const strArr = this._value.split('');
     strArr.splice(0, 0, str);
     const newStr = strArr.join('');
     this._value = newStr;
   }
+  
   pad(str) {
-    // const strArr = this._value.split('');
-    // strArr.splice(0, 0, str);
-    // strArr.splice(strArr.length, 0, str);
-    // const newStr = strArr.join('');
-    // this._value = newStr;
-   
+    
+    this.prepend(str);
+    this.append(str);
   }
 }
 
@@ -34,5 +33,5 @@ console.log(builder.value); // '.^'
 builder.prepend('^');
 console.log(builder.value); // '^.^'
 
- builder.pad('=');
- console.log(builder.value); // '=^.^='
+builder.pad('=');
+console.log(builder.value); // '=^.^='
